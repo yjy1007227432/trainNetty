f3343af372e9ca1a95bcf7fc131f09380a44550c
yaojunyi <1007227432@qq.com>
2022-02-06 23:09:40 +0800

    第一次测试提交

---
+++ src/test/java/com/example/trainnio/TrainNioApplicationTests.java
@@ -0,0 +1,77 @@
+package com.example.trainnio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+@SpringBootTest
+class TrainNioApplicationTests {
+
+    /**
+     * 案例1是采用FileInputStream读取文件内容的
+     */
+    @Test
+    void textIO() {
+        InputStream in = null;
+        try{
+            in = new BufferedInputStream(new FileInputStream("src/nomal_io.txt"));
+            byte [] buf = new byte[1024];
+            int bytesRead = in.read(buf);
+            while(bytesRead != -1)
+            {
+                for(int i=0;i<bytesRead;i++)
+                    System.out.print((char)buf[i]);
+                bytesRead = in.read(buf);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try{
+                if(in != null){
+                    in.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    @Test
+    void textNIO(){
+        RandomAccessFile aFile = null;
+        try{
+            aFile = new RandomAccessFile("src/nio.txt","rw");
+            FileChannel fileChannel = aFile.getChannel();
+            ByteBuffer buf = ByteBuffer.allocate(1024);
+            int bytesRead = fileChannel.read(buf);
+            System.out.println(bytesRead);
+            while(bytesRead != -1)
+            {
+                buf.flip();
+                while(buf.hasRemaining())
+                {
+                    System.out.print((char)buf.get());
+                }
+                buf.compact();
+                bytesRead = fileChannel.read(buf);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(aFile != null){
+                    aFile.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
